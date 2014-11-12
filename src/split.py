01f2ac8fa552c705e81156e94809ee3c6c24ef3e
Cheng LUO <luochengleo@gmail.com>
2014-11-12 22:02:54 +0800

    add

--- src/split.py
+++ src/split.py
@@ -5,7 +5,7 @@ fout = open('../result/longsessions.txt','w')
 count = 0
 for l in open(filepath):
     count +=1
-    if count >= 1000000:
+    if count >= 10000000:
         break
     segs = l.strip().split(' ')
     if len(segs)<5:
@@ -16,20 +16,44 @@ for l in open(filepath):
         for s in segs:
             if 'P@' in s:
                 qlist.append(s.replace('P@',''))
-        if len(set(qlist))>5:
+        if len(set(qlist))>=3:
             try:
                 writen = set()
+                towrite = list()
                 for item in qlist:
                     if item not in writen:
-                        fout.write(item.decode('cp936').encode('utf8') + ' ')
+                        towrite.append(item.decode('cp936').encode('utf8'))
                         writen.add(item)
                     else:
                         pass
 
             except:
                 pass
-            fout.write('\n')
+            if len(towrite) >=3 and checkOverlap(towrite) == True:
+                fout.write(','.join(towrite)+'\n')
         else:
             pass
             # print 'short 2',len(qlist)
 fout.close()
+
+def overlap(item1, item2):
+    all = set()
+    c1 = set()
+    c2 = set()
+    for c in item1:
+        all.add(c)
+        c1.add(c)
+    for c in item2:
+        all.add(c)
+        c2.add(c)
+    if len(all) - len(c1) - len(c2)<0:
+        return True
+    else:
+        return False
+def checkOverlap(lt):
+    for i in range(0,len(lt)-1,1):
+        if overlap(lt[i],lt[i+1]) ==True:
+            continue
+        else:
+            return False
+    return True
